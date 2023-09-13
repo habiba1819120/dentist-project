8cc7903ca0daf711536aa91ca646e1a92cdf78d9
habiba benchamour <habiba.2ben@gmail.com>
2023-09-13 22:23:18 +0100

    test6

--- backend.tf
+++ backend.tf
@@ -1,9 +1,3 @@
 terraform {
-  backend "s3" {
-    bucket         = "your-unique-s3-bucket-name"
-    key            = "terraform.tfstate"
-    region         = "us-east-1"  # Replace with your desired AWS region
-    encrypt        = true         # Enable state file encryption (recommended)
-    
-  }
+
 }
