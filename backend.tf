f2e928503adad889d0e449c764d5af352aa93ff2
habiba benchamour <habiba.2ben@gmail.com>
2023-09-13 20:39:46 +0100

    test

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "pocket-properties-52437-tf-state"
+    bucket = "S3-denstit-tf-files"
     key    = "state/terraform.tfstate"
     region = "us-east-1"
   }
