8b3d58b168e351638f3aff2e494a1c6cf7ce3b0b
habiba benchamour <habiba.2ben@gmail.com>
2023-09-18 12:05:12 +0100

    script

--- ec2/main.tf
+++ ec2/main.tf
@@ -18,9 +18,23 @@ resource "aws_instance" "ec2_instance" {
  user_data = <<-EOF
     #!/bin/bash
     sudo apt-get update
-    sudo apt-get install -y ngnix
-    sudo systemctl start ngnix
-    sudo systemctl enable ngnix
+    # Install OpenSSH if not already installed
+    if ! dpkg -l | grep -q "openssh-server"; then
+      sudo apt-get install -y openssh-server
+    fi
+    # Start and enable OpenSSH service
+    sudo systemctl start ssh
+    sudo systemctl enable ssh
+    # Install Nginx, Docker, AWS CLI, and Java as before
+    sudo apt-get install -y nginx docker.io default-jre
+    sudo apt-get install -y awscli
+    sudo apt-get install -y openjdk-11-jdk
+
+
+    # Configure AWS CLI (replace with your AWS credentials and region)
+    aws configure set aws_access_key_id AKIAV2OBSZ2QGRDPF2K5
+    aws configure set aws_secret_access_key CbnW4Nud8+Thvl2/WgbLiJ7pbs/RKgZfCYs2Y7vp
+    aws configure set region us-east-1
   EOF
 
 }
