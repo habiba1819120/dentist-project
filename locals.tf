335d96b52ff4e701b42023810daa59611ba625f9
habiba benchamour <habiba.2ben@gmail.com>
2023-09-27 16:35:33 +0100

    test

--- locals.tf
+++ locals.tf
@@ -9,7 +9,7 @@ locals {
   prod_ec2s = try(local.prod_env["ec2"], {} )
   v4_env_offset = ceil(log(length(local.environments) + 1, 5))
 
-  rds = try(local.prod_env["rds"], {} )
+  rds = try(local.prod_env["prod-db-postgres"], {} )
 
   prod_aws_eips = try(local.main_vpc["prod_aws_eips"], [])
 }
