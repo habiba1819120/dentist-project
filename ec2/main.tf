75712e864e60b77c6e684ca582afa3470a6d957e
habiba benchamour <habiba.2ben@gmail.com>
2023-09-27 20:05:16 +0100

    last2

--- ec2/main.tf
+++ ec2/main.tf
@@ -12,7 +12,7 @@ resource "aws_instance" "ec2_instance" {
   ami           = var.settings.ami  # Update with appropriate AMI ID
   instance_type =  var.settings.type  # Update with appropriate instance type
   associate_public_ip_address = true
-  subnet_id     = var.subnets.id 
+  subnet_id     = var.subnets[count.index].id 
   tags = {
     Name = var.name
   }
