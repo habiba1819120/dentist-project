e29b375cefc4d790ce816f0c93470ec2e1cb6e59
habiba benchamour <habiba.2ben@gmail.com>
2023-09-27 16:31:57 +0100

    test

--- ec2/main.tf
+++ ec2/main.tf
@@ -1,7 +1,6 @@
 variable "name" {}
 variable "settings" {}
 variable "subnets" {}
-variable "iam_instance_profile" {}
 variable "vpc_security_group_ids" {}
 
 
@@ -13,7 +12,7 @@ resource "aws_instance" "ec2_instance" {
   ami           = var.settings.ami  # Update with appropriate AMI ID
   instance_type =  var.settings.type  # Update with appropriate instance type
   associate_public_ip_address = true
-  subnet_id     = var.subnets[count.index].id 
+  subnet_id     = var.subnets.id 
   tags = {
     Name = var.name
   }
