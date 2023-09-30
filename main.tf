fdf57a92c4e8a5ac3f0bb803b20bf2c39f1446f1
habiba benchamour <habiba.2ben@gmail.com>
2023-09-30 15:25:33 +0100

    az for subnet

--- main.tf
+++ main.tf
@@ -26,15 +26,15 @@ resource "aws_vpc" "main_vpc" {
 data "aws_availability_zones" "az" {
   state = "available"
 }
-resource "aws_subnet" "rds_subnet" {
-  count = length(local.rds) 
+esource "aws_subnet" "rds_subnet" {
+  count = length(local.rds)
 
-  cidr_block = "10.0.1.0/24" #cidrsubnet(cidrsubnet(local.main_vpc.cidr, local.v4_env_offset,0), local.v4_env_offset+count.index,0) 
+  cidr_block = "10.0.${count.index}.0/24"
   vpc_id     = aws_vpc.main_vpc.id
-  availability_zone = "us-east-1a"
+  availability_zone = data.aws_availability_zones.az.names[count.index % length(data.aws_availability_zones.az.names)]
 
   tags = {
-    Name = "rds-${count.index + 1}"
+    Name = "rds-${count.index + 1}" 
   }
 }
 resource "aws_db_subnet_group" "custom_db_subnet_group" {
