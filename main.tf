9ee21d38e664ede8b258a16a67f1c1e3b6ade16e
habiba benchamour <habiba.2ben@gmail.com>
2023-09-30 14:57:34 +0100

    az for rds , engine version

--- main.tf
+++ main.tf
@@ -27,20 +27,18 @@ data "aws_availability_zones" "az" {
   state = "available"
 }
 resource "aws_subnet" "rds_subnet" {
-  count = length(local.rds)
 
   cidr_block = "10.0.0.0/24" #cidrsubnet(local.main_vpc.cidr, local.v4_env_offset+count.index,0) 
   vpc_id     = aws_vpc.main_vpc.id
-  availability_zone = data.aws_availability_zones.az.names[count.index]
-
+  availability_zone = "us-east-1a"
   tags = {
-    Name = "rds-${count.index + 1}" 
+    Name = "rds-subnet" 
   }
 }
 resource "aws_db_subnet_group" "custom_db_subnet_group" {
   name       = "my-custom-db-subnet-group"
   description = "Custom DB Subnet Group"
-  subnet_ids = aws_subnet.rds_subnet[*].id
+  subnet_ids = aws_subnet.rds_subnet.id
 }
 resource "aws_subnet" "prod_subnet" {
   count = length(local.prod_ec2s)
